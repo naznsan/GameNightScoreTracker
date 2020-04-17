9bfd8b9d8adf1bd679962797d4e8354a54d2fb84
Justin Kim <38806488+naznsan@users.noreply.github.com>
2020-04-17 16:51:18 +1200

    Starting on rows of scores

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function Player(name) {
 	this.name = name;
-	this.scores = [];
+	this.scores = [2, 4, 5];
 }
 
 function addPlayer() {
@@ -38,9 +38,26 @@ function populateTableNames() {
 		let row = table.insertRow();
 		let cell = row.insertCell(0);
 		cell.innerHTML = player.name;
-	})
-
+	});
 }
+
+
+function populateScores() {
+	const table = document.getElementById("scores-table");
+	for (let player of playersList) {
+		let row = table.insertRow();
+		if (player.scores.length == 0) {
+			let cell = row.insertCell(0);
+			cell.innerHTML = 0;
+		} else {
+			player.scores.forEach(score => {
+				let cell = row.insertCell(0);
+				cell.innerHTML = score;
+			})			
+		}
+	}
+}
+
 
 var playersList = [];
 playersList.push(new Player("Player1"));
@@ -48,3 +65,4 @@ playersList.push(new Player("Player2"));
 playersList.push(new Player("Player3"));
 
 populateTableNames();
+populateScores();
